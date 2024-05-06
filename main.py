ab938377236baef3696d9dd2b69aec66d7652a19
FELIX EDUARDO MIRANDA TERAN <feedmite@hotmail.com>
2024-05-06 00:48:39 -0500

    openai integration

--- main.py
+++ main.py
@@ -16,6 +16,56 @@ async def root():
     return {"Message": "api running"}
 
 
+from langchain_openai import ChatOpenAI
+from openai import OpenAI
+
+#
+
+llm = ChatOpenAI()
+
+
+from dotenv import find_dotenv, load_dotenv
+
+_ = load_dotenv(find_dotenv())  # read local .env file
+
+client = OpenAI()
+
+llm_model = "gpt-3.5-turbo"
+
+
+def get_completion(prompt: str, model=llm_model):
+    messages = [{"role": "user", "content": prompt}]
+    response = client.chat.completions.create(
+        model=model, messages=messages, temperature=0  # type: ignore
+    )  # type:ignore
+    print(response.choices[0].message.content)
+
+
+# get_completion("What is 1+1?")
+
+
+customer_email = """
+Arrr, I be fuming that me blender lid \
+flew off and splattered me kitchen walls \
+with smoothie! And to make matters worse,\
+the warranty don't cover the cost of \
+cleaning up me kitchen. I need yer help \
+right now, matey!
+"""
+style = """Colombian Paisa in a angry and disrespectful tone"""
+
+prompt = f"""Translate the text \
+that is delimited by triple backticks
+into a style that is {style}.
+text: ```{customer_email}```
+"""
+get_completion(prompt)
+# response = get_completion(prompt)
+
+
+# print(response)
+
+
 @app.post("/images/upload")
 async def remove_bg(file: UploadFile = File(...)):
     temp_dir = "temp"
